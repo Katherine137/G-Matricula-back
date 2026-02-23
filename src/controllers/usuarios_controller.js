85580c025c58c5d94aa7218a42b50d148ecb9285
Katherine137 <katherine.sailema@epn.edu.ec>
2026-02-22 21:13:41 -0500

    actualizado

--- src/controllers/usuarios_controller.js
+++ src/controllers/usuarios_controller.js
@@ -3,39 +3,39 @@ import jwt from "jsonwebtoken"
 
 const registro = async (req,res) =>{
     try {
-        const {email, password} = req.body
-        if (Object.values(req.body).includes("")) return res.status(400).json({msg: "lo sentimos, debe llenar todos los campos"})
+        const {email,password} = req.body
+        if (Object.values(req.body).includes("")) return res.status(400).json({msg:"lo sentimos, debe llenar todos los campos"})
         const verificarEmailBDD = await Usuarios.findOne({email})
-        if(verificarEmailBDD) return res.status(400).json({msg: "lo sentimos, el email ya se encuentra registrado"})
+        if(verificarEmailBDD) return res.status(400).json({msg:"lo sentimos, el email ya se encuentra registrado"})
         const nuevoUsuario = new Usuarios(req.body)
         nuevoUsuario.password = await nuevoUsuario.encryptPassword(password)
         await nuevoUsuario.save()
-        res.status(201).json({msg: "Usuario registrado correctamente"})
+        res.status(201).json({msg:"Usuario registrado correctamente"})
     } catch (error) {
-        res.status(500).json({msg: `❌ Error en el servidor - ${error}`})
+        res.status(500).json({msg:`❌ Error en el servidor - ${error}`})
     }
 }
 
 const login = async (req,res) =>{
     try {
-        const {email, password} = req.body
-        if(Object.values(req.body).includes("")) return res.status(400).json({msg: "lo sentimos, debe llenar todos los campos"})
-        const UsuariosBDD = await Usuarios.findOne({email})
-        if(!UsuariosBDD) return res.status(401).json({msg: "lo sentimos, el email no se encuentra registrado"})
-        const verificarPassword = await UsuariosBDD.matchPassword(password)
-        if(!verificarPassword) return res.status(401).json({msg: "lo sentimos, la contraseña es incorrecta"})
-        const token = jwt.sign({id:UsuariosBDD._id}, process.env.JWT_SECRET, {expiresIn: '1h'})
+        const {email,password} = req.body
+        if(Object.values(req.body).includes("")) return res.status(400).json({msg:"lo sentimos, debe llenar todos los campos"})
+        const UsuarioBDD = await Usuarios.findOne({email})
+        if(!UsuarioBDD) return res.status(401).json({msg:"lo sentimos, el email no se encuentra registrado"})
+        const verificarPassword = await UsuarioBDD.matchPassword(password)
+        if(!verificarPassword) return res.status(401).json({msg:"lo sentimos, la contraseña es incorrecta"})
+        const token = jwt.sign({id:UsuarioBDD._id}, process.env.JWT_SECRET, {expiresIn: '1h'})
         res.status(200).json({
             token,
-            Usuarios:{
-                id: UsuariosBDD._id,
-                nombre: UsuariosBDD.nombre,
-                apellido: UsuariosBDD.apellido,
-                email: UsuariosBDD.email
+            usuario:{
+                id: UsuarioBDD._id,
+                nombre: UsuarioBDD.nombre,
+                apellido: UsuarioBDD.apellido,
+                email: UsuarioBDD.email
             }
         })
     } catch (error) {
-        res.status(500).json({msg: `❌ Error en el servidor - ${error}`})
+        res.status(500).json({msg:`❌ Error en el servidor - ${error}`})
     }
 }
 
@@ -52,7 +52,7 @@ const perfil = async (req,res) =>{
             }
         })
     } catch (error) {
-        res.status(500).json({msg: `❌ Error en el servidor - ${error}`})
+        res.status(500).json({msg:`❌ Error en el servidor - ${error}`})
     }
 }
 
