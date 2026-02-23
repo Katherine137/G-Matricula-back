cf79875bc04b400f1863f006a0330c9b2fd4c81a
Katherine137 <katherine.sailema@epn.edu.ec>
2026-02-22 21:03:42 -0500

    actualizado

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ dotenv.config();
 import app from "./server.js";
 import connection from "./database.js";
 
-const startServer = async () =>{
-    try {
-        await connection()
-        app.listen(app.get('port'), ()=>{
-            console.log(`Server ok on port ${app.get('port')}`)
-        })
-    } catch (error) {
-        console.log("Error starting server: ", error)
-    }
+const startServer = async () => {
+  try {
+    await connection()
+    app.listen(app.get('port'), () => {
+      console.log(`Server ok on port ${app.get('port')}`)
+    })
+  } catch (error) {
+    console.log("Error starting server:", error)
+  }
 }
 
 startServer()
